3c129ffaf587e6e5f6fcdf8c62a45c3ae5a752d6
Prince-Bolo <princetadle.bolo@my.smciligan.edu.ph>
2026-07-03 11:40:05 +0800

    Add files via upload

---
+++ IT 400/components/footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+    <footer
+    style={{
+    backgroundColor: "#1f2937",
+    color: "white",
+    textAlign: "center",
+    padding: "15px",
+    marginTop: "20px",
+    }}
+    >
+    <p>&copy; 2026 Student Portal. All Rights Reserved.</p>
+    <p>Developed by BSIT Students</p>
+    </footer>
+    );
+    }
+    export default Footer;
